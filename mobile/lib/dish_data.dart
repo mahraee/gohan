2d8beeeb963ef0ac7e17dc2ba77299e892793808
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2021-06-17 16:31:01 +0200

    adding dish data and screen tabs in screen

---
+++ mobile/lib/dish_data.dart
@@ -0,0 +1,100 @@
+import 'dish.dart';
+
+const AllDishes = const [
+  Dish(
+      id: '1',
+      title: 'Om Ali',
+      cal: '612.22',
+      ingredients:
+          "1 (17.5 oz) package frozen puff pastry sheets, thawed\n1/2 cup chopped walnuts\n1 cup chopped pecans\n1 cup chopped hazelnuts\n1 cup raisins\n1 cup flaked coconut\n1 1/4 cups white sugar, divided\n4 cups milk\n1/2 cup heavy cream",
+      steps:
+          "Preheat oven to 350 degrees F (175 degrees C). Butter a 9x13 inch baking dish.\nPlace the pastry sheets in the baking dish and place the dish in the oven. Watch it closely. When the top layer turns crunchy and golden, remove it from the oven. Continue until all the sheets are cooked.\nPreheat the oven broiler.\nIn a bowl, combine walnuts, pecans, hazelnuts, raisins, coconut and 1/4 cup sugar. Break cooked pastry into pieces and stir into nut mixture. Spread mixture evenly in 9x13 inch dish.\nBring milk and 1/2 cup sugar to a boil in a medium saucepan over medium heat. Pour over nut mixture.\nBeat the heavy cream with the remaining 1/2 cup sugar until stiff peaks form. Spread evenly over nut mixture in dish.\nPlace dessert under oven broiler until top is golden brown, about 10 minutes. Serve hot."),
+  Dish(
+      id: '2',
+      title: 'Egyptian Koshari',
+      cal: '200',
+      ingredients:
+          "1 ½ cup brown lentils, picked over and well-rinsed \n 1 ½ cup medium-grain rice, rinsed, soaked in water for 15 minutes,drained\n½ tsp each salt and pepper \n½ tsp coriander\n2 cups elbow pasta\nCooking oil\nWater\n1 15-oz can chickpeas, rinsed, drained and warmed\n tomato souce",
+      steps:
+          "Cook the lentils. \n Bring lentils and 4 cups of water to a boil in a medium pot or saucepan over high heat. \nReduce the heat to low and cook until lentils are just tender (15-17 minutes). \nDrain from water and season with a little salt. \n(Note: when the lentils are ready, they should not be fully cooked. They should be only par-cooked and still have a bite to them as they need to finish cooking with the rice).\nNow, for the rice. Drain the rice from its soaking water. Combine the par-cooked lentils and the rice in the saucepan over medium-high heat with 1 tbsp cooking oil, salt, pepper, and coriander. Cook for 3 minutes, stirring regularly.\n Add warm water to cover the rice and lentil mixture by about 1 ½ inches (you'll probably use about 3 cups of water here).\n Bring to a boil; the water should reduce a bit. \nNow cover and cook until all the liquid has been absorbed and both the rice and lentils are well cooked through (about 20 minutes). \nKeep covered and undisturbed for 5 minutes or so.\nNow make the pasta. \nWhile the rice and lentils are cooking, make the pasta according to package instructions by adding the elbow pasta to boiling water with a dash of salt and a little oil. Cook until the pasta is al dente. Drain.\nCover the chickpeas and warm in the microwave briefly before serving."),
+  Dish(
+      id: '3',
+      title: 'Taameya',
+      cal: '234',
+      ingredients:
+          "2 cups dried split fava beans\n1 red onion, quartered\n½ cup fresh parsley\n½ cup fresh cilantro\n½ cup fresh dill\n3 cloves garlic\n1 ½ teaspoons ground coriander\n1 ½ teaspoons salt\n1 teaspoon ground cumin\n1 cup sesame seeds (Optional)\nvegetable oil for frying",
+      steps:
+          "Place fava beans in large bowl and cover with several inches of water. Let soak, 8 hours to overnight. Drain.\nCombine soaked fava beans, red onion, parsley, cilantro, dill, garlic, coriander, salt, and cumin in a food processor; process to a dough-like consistency.\nHeat a skillet over medium heat. Add sesame seeds; cook, stirring occasionally, until toasted, about 5 minutes. Transfer to a large plate.\nShape fava bean mixture into balls. Roll in sesame seeds to coat.\nFill a large saucepan 1/4 full with oil; heat over medium heat.\nFry fava bean balls in batches until golden brown, 3 to 5 minutes. Drain on paper towels."),
+  Dish(
+      id: '4',
+      title: 'Stuffed grape leaves',
+      cal: '63',
+      ingredients:
+          "1 can of Grape Leaves \n1 bunch Parsley\n2 cup Rice\n2 can Tomato paste\n1 Salt to taste\n1/2 Pepper\n1 large onion\n1 Chicken broth",
+      steps:
+          "Food process large onion into paste. \nMix onions, rice, parsley, paste, salt and pepper together. \nClean grape leaves with water. Prepare them on a plate. \nDepending on the size of the grape leaf put as much rice mixture as needed in the middle of the league and roll the end in and then fold the sides in and roll tightly till the end.\nRepeat. Pack grape leaves tightly together in pot.\nAdd chicken broth 1inch above grape leaves.\nBring to boil and then cover and put on low heat until rice is cooked."),
+  Dish(
+      id: '5',
+      title: 'Egyptian Molokhia ',
+      cal: '110',
+      ingredients:
+          "Chicken broth or rabbit\nFrozen Molokhia\nDry coriander\nGarlic\nGhee\nBullion cube",
+      steps:
+          "In a deep pot over medium-high heat melt ghee.\nSauté garlic for 15-30 seconds.until lightly golden and fragrant\nAdd coriander and keep stirring for another 30 seconds.\nThe coriander will give its fragrance and turn a little more dark brown.\nPour in some of the broth keeping the rest after we check consistency at the end.\nSprinkle the bouillon cube. Stir until it is dissolved.\nAdd the Molokhia, and turn down the heat to medium low, keep stirring every now and then.\nMolokhia will slowly melt in the broth. Wait until it has completely melted.\nCheck consistency: if it is too thick for your liking add more broth.\nCheck the seasoning and sweetness: if you need salt."),
+  Dish(
+      id: '6',
+      title: 'konafa',
+      cal: '255',
+      ingredients:
+          "½ kilo of konafa.\n½ kilo of melted butter.\n2 cups of milk.\n3 tablespoons of starch.\nA cup of sweet cream.\n½ cup of cream.\n2 tablespoons of sugar.\n2 cups Syrup.",
+      steps:
+          "Mix the starch with cold milk in a saucepan, then put it on the fire while continuing to stir until the mixture becomes a little thick.\nAdd the sweet cream and continue to stir for 3 minutes, then remove the mixture from the heat and add the cream, then stir the mixture until the ingredients mix well, and leave the mixture aside to cool.\nPut the kunafa in a large bowl, add the melted ghee and mix well.\nBring the oven tray and put half the amount of kunafa in it, then add the previously prepared cream and put the other half of the kunafa.\nPut the tray in the oven at a temperature of 200 degrees until it turns golden.\nRemove the tray from the oven, then add the syrup to it, ready to serve."),
+  Dish(
+      id: '7',
+      title: 'Qataif',
+      cal: '664',
+      ingredients:
+          "1 cup all-purpose flour\n1 cup water\n½ cup milk\n½ cup fine semolina\n1 teaspoon baking powder\n½ teaspoon instant yeast\n2 tablespoons sugar\n2 teaspoon pure vanilla extract\n½ teaspoon salt",
+      steps:
+          "To prepare the mixture, place all of the dry ingredients in a bowl and mix.\nWith a whisk, mix the wet ingredients then gradually combine them with the dry ingredients. Give everything a good whisk until well combined.\nCover with a towel, and let the mixture sit in a warm place for 20-30 minutes.\nHeat a non-stick pan on medium heat, then pour the batter from a jug or using a spoon to create little circles (for small Atayef pour 1 tablespoon of batter, for medium sized ones add 2 tablespoons, and ¼ cup for the large ones).\nAs the Atayef cook, lots of bubbles will appear. Keep cooking (do not flip) until these bubbles dry out (about 2 minutes). If no bubbles are appearing, you might need to add a little bit of more water as your batter is probably too thick.\nWhen the bubbles dry out completely and are no longer shiny, it means that the atayef are done. Remove them from pan immediately, and repeat the process until you finish up the batter.\nTransfer the atayef to a tea towel and cover very well so that they don't dry out (otherwise they will start breaking when you try to fold/seal them).\nStuff the Atayef with the filling of your choice (cheese, cinnamon walnuts, or almonds) then either deep fry them, or brush them with oil and bake until they're golden and crispy. Dip in sugar syrup or just drizzle with it (for best results, soak warm Atayef in cold syrup)."),
+  Dish(
+      id: '8',
+      title: 'lentil soup',
+      cal: '196',
+      ingredients:
+          "3 cups water\n1 cup red lentils\n1 roma tomato, quartered\n1 carrot, quartered\n1 small onion, quartered\n4 cloves garlic, quartered\n¼ cube chicken bouillon (such as Maggi®)\n1 cup water\n2 teaspoons ground cumin\n½ teaspoon sea salt\n½ teaspoon cracked black pepper\n¼ teaspoon ground coriander",
+      steps:
+          "Place 3 cups water, lentils, tomato, carrot, onion, garlic, and chicken bouillon in a stockpot over medium heat; cook until vegetables and lentils are softened, 20 to 25 minutes. Remove from heat and cool to lukewarm.\nBlend vegetable and lentil mixture with an immersion blender until smooth. Stir 1 cup water, cumin, sea salt, pepper, and coriander into soup; heat over medium heat until warmed."),
+  Dish(
+      id: '9',
+      title: 'Egyptian Goulash',
+      cal: '220',
+      ingredients:
+          "16 oz. package of frozen phyllo dough, thawed in its package\n 1 egg, whisked in 1 cup of milk\n1 cup Private Reserve extra virgin olive oil OR ½ cup olive oil \ncombined with ½ cup melted butter\n1 cup chopped onions\n1 ½ lb lean ground beef\n1 tsp ground allspice\n1 tsp garlic powder\nSalt and pepper",
+      steps:
+          "Preheat the oven to 350 degrees F.\nCarefully lay the thawed phyllo dough sheets between two lightly damp kitchen towels.\nBegin with preparing the meat filling. Heat 1 tbsp olive oil in a large nonstick pan. On medium-high heat, saute the chopped onions briefly. Add the ground beef. Cook until fully browned, tossing regularly. Remove the pan from the heat briefly to drain any fat, then return back to the stove. Add in the spices and stir to fully combine. \nRemove from the heat.\nNow, to assemble the phyllo pie, lay three to four sheets of phyllo dough on the bottom of a lightly greased 9 ½ x 13 x 2 ¼ inch baking pan. Fold any excess dough in. \nBrush the top phyllo sheet very generously with the melted butter and oil mixture.\nRepeat this step as you lay three to four sheets of phyllo at a time in the baking pan until you have used up ½ of the phyllo sheets.\nNow, as you have a nice base for your pie, spread the meat filling evenly on top of the last phyllo sheet.\nLayer the remaining sheets on top of the meat repeating the same process as before (again, three or four sheets at a time, fold excess phyllo and butter the top sheet) until the phyllo sheets are used up.\nBrush the top sheet very generously with the butter and olive oil mixture. With a sharp knife, cut the phyllo pie into 12 squares.\nSeason the prepared milk and egg mixture with salt and pepper and pour evenly over the pie.\nPlace the phyllo meat pie on the middle rack of the 350 degrees F heated-oven for about 45 minutes to an hour, or until cooked through. \nIt should turn a nice golden brown. Be watchful so that pie does not over bake or burn."),
+  Dish(
+      id: '10',
+      title: 'Asabe Zeinab',
+      cal: '240',
+      ingredients:
+          "250 gm white flour\n100 gm semolina basbousa\n2 tablespoons ghee\n2 tablespoons of cold-pressed oil\n1 teaspoon instant yeast\npinch of salt\ntablespoon sugar\n4/3 cup warm water\nHot oil for frying\nHeavy syrup \n3 cups of sugar\nA cup and a quarter of water\nLemon juice\nvanilla",
+      steps:
+          "First we prepare the syrup, we put sugar and water on the fire without stirring and after boiling we add lemon juice and leave it to boil for about 15 minutes until it is thick and after we turn off the fire add the vanilla so that we retain its aroma.\nThen add the white flour, semolina, yeast, sugar and a pinch of salt in a different bowl, then grind two tablespoons of oil and leave them until they cool completely, and add the oil to the dry ingredients with two tablespoons of ghee at room temperature and knead the ingredients while adding warm water gradually. \nYou can use only 1/2 cup of water so that it does not stick. \n The dough with hands, cover it and leave it for half an hour to rest, then cut it into equal-sized balls and shape them as fingers, and we spread the finger on the grater to get a distinctive shape and so on, then cover it with plastic and leave it for an hour and fry in hot and abundant oil until it takes a golden color and put in heavy sherbet for 3 minutes Until you drink it well, then filter it with a colander to get rid of the excess syrup and serve in a dish."),
+  Dish(
+      id: '11',
+      title: 'Ful Medames',
+      cal: '240',
+      ingredients:
+          "3 cloves garlic\n1 teaspoon cumin seeds, freshly toasted\nKosher salt\n2 (15-ounce) cans fava beans\n3 tablespoons tahini\n2 to 3 tablespoons lemon juice from 2 lemons, or more to taste",
+      steps:
+          "Put garlic cloves, cumin seeds, and a pinch of salt in a mortar and pestle and crush until seeds are cracked and garlic is in small, flimsy chunks. If you don't have a mortar and pestle, mince garlic very fine.\nEmpty fava beans (with liquid) into a medium saucepan and combine with tahini and garlic paste. Cook over medium-high heat, stirring frequently, until liquid retains some brothiness but turns thick and sauce-like, about 5 minutes.\nAdd lemon juice and salt to taste. Mash one third of the beans with a potato masher to thicken if desired, then serve with toasted pita."),
+  Dish(
+      id: '12',
+      title: 'Fattah',
+      cal: '680',
+      ingredients:
+          "1 Onion, red, cut into half\n100 ml Olive oil \n3 kg Beef shank\n200 g Onion, finely grated\n5 pc Cardamom, whole\n5 g Black pepper\n3 pc Bay Leaves\n5 g Rosemary\n200 g Tomatoes, diced\n2 l Water\n50 g Beef Flavoured Stock Powder (6x1.1Kg) \n10 g Brown Gravy Base (6x800g)\n5 g Pita bread, cut into cubes\n100 g Butter\n50 g Garlic, crushed\n150 gWhite Vinegar\n1 kg Rice, Egyptian, cooked",
+      steps:
+          "On open flame char the red onion until it turns dark, but not burned. Set aside\nIn a large cooking pot warm olive oil over medium heat, add the shanks and cook a few minutes until brown in color. Remove the shanks and set aside.\nIn the same cooking pot over low-medium heat sauté onion, then add the spices, rosemary and tomatoes and cook for another 3 minutes or so, again stirring often.\nAdd back the beef. Add water,  Beef Stock Powder and  Brown Gravy.\nIncrease heat and bring to boil then cover and reduce heat to a simmer allow to cook for 30-40 minutes.\nIn a baking sheet spread the bread and bake at 170°C for 7 minutes until it crispy.\nIn a sauté pan melt the butter, then add the garlic and cook until it's golden brown add the white vinegar.\nHeat the rice before layering.\nIn a serving platter put the bread first then rice then the garlic vinegar mixture.\nTop with the beef shanks and drizzle it with the broth."),
+];
